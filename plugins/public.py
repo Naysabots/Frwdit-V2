4097c92e97c311d56d39e01bb3aebdbaa8c7c0de
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-02 11:54:20 +0530

    Update public.py

--- plugins/public.py
+++ plugins/public.py
@@ -10,7 +10,21 @@ from pyrogram.errors import FloodWait
 from config import Config
 from translation import Translation
 
-
+# filters for auto post
+FILTER_TEXT = bool(os.environ.get("FILTER_TEXT", True))
+FILTER_AUDIO = bool(os.environ.get("FILTER_AUDIO", True))
+FILTER_DOCUMENT = bool(os.environ.get("FILTER_DOCUMENT", True))
+FILTER_PHOTO = bool(os.environ.get("FILTER_PHOTO", True))
+FILTER_STICKER = bool(os.environ.get("FILTER_STICKER", True))
+FILTER_VIDEO = bool(os.environ.get("FILTER_VIDEO", True))
+FILTER_ANIMATION = bool(os.environ.get("FILTER_ANIMATION", True))
+FILTER_VOICE = bool(os.environ.get("FILTER_VOICE", True))
+FILTER_VIDEO_NOTE = bool(os.environ.get("FILTER_VIDEO_NOTE", True))
+FILTER_CONTACT = bool(os.environ.get("FILTER_CONTACT", True))
+FILTER_LOCATION = bool(os.environ.get("FILTER_LOCATION", True))
+FILTER_VENUE = bool(os.environ.get("FILTER_VENUE", True))
+FILTER_POLL = bool(os.environ.get("FILTER_POLL", True))
+FILTER_GAME = bool(os.environ.get("FILTER_GAME", True))
 files_count = 0
 
 #===================Run Function===================#
