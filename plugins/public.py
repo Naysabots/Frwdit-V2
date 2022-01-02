37a39073da9031ed6a0597b450e44f6bbfbe4fe4
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-02 08:41:33 +0530

    Update public.py

--- plugins/public.py
+++ plugins/public.py
@@ -10,7 +10,7 @@ from pyrogram.errors import FloodWait
 from config import Config
 from translation import Translation
 
-FILTER = Config.FILTER_TYPE
+FILTER = document , audio , photo , video , animation
 files_count = 0
 
 #===================Run Function===================#
